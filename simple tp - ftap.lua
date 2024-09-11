18ec7fe1e910d12acced88755d88ce6d37491d94
m1kpe0 <yarikasik@inbox.ru>
2024-09-11 20:36:49 +0300

    Create simple tp - ftap.lua

---
+++ simple tp - ftap.lua
@@ -0,0 +1,24 @@
+--либрари
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/lime"))()
+--вкладка
+local w = Library:Window("тп к домам")
+--кнопки
+w:Button("розовый дом", function()
+   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-491, -7, -166)
+end)
+
+w:Button("рзеленый дом", function()
+   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-535, -7, 93)
+end)
+
+w:Button("фиолетовый дом", function()
+   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(250, -6, 463)
+end)
+
+w:Button("китайский дом", function()
+   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(554, 123, -72)
+end)
+
+w:Button("голубой дом", function()
+   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(510, 83, -339)
+end)
