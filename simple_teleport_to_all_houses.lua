0ebfbc132b2ae70928ebf3156138f3699bd463e3
m1kpe0 <yarikasik@inbox.ru>
2024-11-01 18:21:55 +0300

    Update simple_teleport_to_all_houses.lua

--- simple_teleport_to_all_houses.lua
+++ simple_teleport_to_all_houses.lua
@@ -17,24 +17,24 @@
 --либрари
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/lime"))()
 --вкладка
-local w = Library:Window("тп к домам")
+local w = Library:Window("teleport")
 --кнопки
-w:Button("розовый дом", function()
+w:Button("pink house", function()
    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-491, -7, -166)
 end)
 
-w:Button("зеленый дом", function()
+w:Button("green house", function()
    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-535, -7, 93)
 end)
 
-w:Button("фиолетовый дом", function()
+w:Button("purple house", function()
    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(250, -6, 463)
 end)
 
-w:Button("китайский дом", function()
+w:Button("china house", function()
    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(554, 123, -72)
 end)
 
-w:Button("голубой дом", function()
+w:Button("blue house", function()
    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(510, 83, -339)
 end)
