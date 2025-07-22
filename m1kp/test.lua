7a84aea30502620fe8acf0b134aa24e193ca6a4b
m1kpe0 <yarikasik@inbox.ru>
2025-07-22 13:52:04 +0300

    Create test.lua

---
+++ m1kp/test.lua
@@ -0,0 +1,21 @@
+local Module = {}
+Module.__index = Module
+
+local Players = game:GetService("Players")
+local Me = Players.LocalPlayer
+
+function Module.new(Owners: number, OwnerName: string)
+   local self = setmetatable({}, Module)
+   self.OwnerId = Owners or nil
+   self.OwnerName = OwnerName or nil
+
+   return self
+end
+
+function Module:CheckOwner(self, UserId: number)
+   if self.OwnerId == UserId then
+      return true
+   end
+
+   return false
+end   
