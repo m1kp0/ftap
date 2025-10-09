8d39bfebfb0ba4a0b4b01cc30b624e36387f1b14
m1kpe0 <yarikasik@inbox.ru>
2025-10-09 19:00:09 +0300

    Update m1kp_loader.lua

--- m1kp/m1kp_loader.lua
+++ m1kp/m1kp_loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/d3fffd5f43514cda86b37feb3fe1bd47"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/aaf1590e7c8b4464a1c60b59876cbe65"))()
