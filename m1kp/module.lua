5823ea59682fb017eaf48ca62d941ee57e5e5295
m1kpe0 <yarikasik@inbox.ru>
2025-10-09 23:40:49 +0300

    Update module.lua

--- m1kp/module.lua
+++ m1kp/module.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/195f3b07322642aa83f680214609069e"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/d158f28af0e442a5a6f150d514450908"))()
